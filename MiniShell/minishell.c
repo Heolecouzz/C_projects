65a4770654d1a7c0f2171d761acfd9f1d5a7c0ba
Gelineau <art.gln8@gmail.com>
2026-08-13 02:40:12 -0700

    Creation of easy command

--- MiniShell/minishell.c
+++ MiniShell/minishell.c
@@ -2,41 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#define MAX_CMD_LEN 1024 // Maximum length of the CLI
+#define MAX_ARGS 64      // Maximum number of arguments
+
+// Function that takes a string and create an array of commands
+void parse_command(char* input, char** args) {
+
+    int i = 0;
+    char* token = strtok(input, " \t\n");
+
+    while (token != NULL && i < MAX_ARGS - 1) {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " \t\n");
+    }
+    
+    // execvp needs that the array finishes by NULL
+    args[i] = NULL; 
+}
+
+
+int lengthArray(char** args) {
+
+    int count = 0;
+    int arrayLen = 0;
+    while (args[count] != NULL) {
+        arrayLen++;
+        count++;
+    }
+
+    return arrayLen;
+}
 
 int main() {
 
     int finished = 0;
-    int commandLength = 100;
-    int commandArrayLentgh = 50;
-    char* command = malloc (commandLength * sizeof(char));
-    char* commandArray = malloc(commandArrayLentgh * sizeof(char));
+    char command[MAX_CMD_LEN];
+    char* args[MAX_ARGS]; // Array of pointers to words of the command
 
     while (!finished) {
 
         printf("> ");
-        fgets(command, commandLength, stdin);
-        int index = strcspn(command, "\n");
-        printf("\nIndex = %d", index);
-        //command[strcspn(command, "\n")] = '\0';
+        if (fgets(command, MAX_CMD_LEN, stdin) == NULL) {
+            printf("\n");
+            break; 
+        }
 
-        if (strcmp(command, "exit") == 0) {
-            printf("Goodbye, see you soon !");
-            free(commandArray);
-            exit(EXIT_SUCCESS);
-        } else {
+        parse_command(command, args);
+        int arrayLen = lengthArray(args);
 
-            printf("Command will be processed soon");
-            
-            // pid_t pid = fork();
-
-            //if (pid == 0) {
-                // Son part
-
-            //}
+        if (strcmp(args[0], "exit") == 0) {
+            printf("Goodbye, see you soon !\n");
+            break;
+        } 
+        else {
+            for (int i = 0; i < arrayLen; i++) {
+                printf("[Debug] Command part %d : %s\n", i, args[i]);
+            }
         }
     }
 
-    free(command);
-    free(commandArray);
+    return EXIT_SUCCESS;
 }
